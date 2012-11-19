6bcc425e2ede25f3c358c53f45d3af88383738cc
Thiago Miranda <tamsmiranda@gmail.com>
2012-11-19 20:54:55 -0200

    Atualizando nomes no admin/forsales.php e .xml

--- administrator/forsales.php
+++ administrator/forsales.php
@@ -9,11 +9,11 @@
 
 defined('_JEXEC') or die;
 
-if (!JFactory::getUser()->authorise('core.manage', 'com_weblinks'))
+if (!JFactory::getUser()->authorise('core.manage', 'com_forsales'))
 {
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-$controller	= JControllerLegacy::getInstance('Weblinks');
+$controller	= JControllerLegacy::getInstance('Forsales');
 $controller->execute(JFactory::getApplication()->input->get('task'));
 $controller->redirect();
